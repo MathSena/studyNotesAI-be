59faac8c435bd297dfe62d614c7df60b40d999a4
MathSena <MathSena07@hotmail.com>
2024-12-14 22:36:44 -0300

    Initial commit

---
+++ src/main/java/com/mathsena/studynotesai/StudyNotesAiBeApplication.java
@@ -0,0 +1,13 @@
+package com.mathsena.studynotesai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyNotesAiBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyNotesAiBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mathsena/studynotesai/StudyNotesAiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathsena.studynotesai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyNotesAiBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
